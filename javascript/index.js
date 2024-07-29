0703631bc691e306838719387eeaea3ae38d0342
JorgeLeon066 <almendrasj315@gmail.com>
2024-07-28 23:21:43 -0300

    encriptador funcional. detalles de estilo y validacion de requisitos de texto

--- javascript/index.js
+++ javascript/index.js
@@ -7,29 +7,52 @@ No deben ser utilizados letras con acentos ni caracteres especiales
 
 Debe ser posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original
 */
-let llave = ["enter", "imes", "ai", "ober", "ufat"];
+const key = ["enter", "imes", "ai", "ober", "ufat"];
+
+const texto = document.getElementById("textarea-input");
+const outCrypt = document.getElementById('output-message');
+const messageNone = document.getElementById('message-none');
 
 //Funcionalidades
 
-const encrypt = (txt) => {
-  let outEncrypt = document.getElementById('output-message');
-  let messageNone = document.getElementById('message-none');
-
-  for (i = 0; i < llave.length; i++) {
-    txt = txt.replaceAll(llave[i][0], llave[i]);
-  };
-
+const send = (txt) => {
   messageNone.style.display = "none";
-  outEncrypt.innerText = txt;
+  outCrypt.innerText = txt;
+}
+
+const encrypt = (txt) => {
+  for (i = 0; i < key.length; i++) {
+    txt = txt.replaceAll(key[i][0], key[i]);
+  };
+  return txt;
 };
+
+const decrypt = (txt) => {
+  for (i = 0; i < key.length; i++) {
+    txt = txt.replaceAll(key[i], key[i][0]);
+  };
+  return txt;
+}
+
+const crypt = (type, txt_edit) => {
+  send(
+    type === "encrypt" && encrypt(txt_edit) ||
+    type === "decrypt" && decrypt(txt_edit)
+  );
+}
+
 
 function starEncrypt () {
-  let texto = document.getElementById("textarea-input");
-  encrypt(texto.value)
+  crypt("encrypt", texto.value)
 };
 
-function starDecrypt () {};
+function starDecrypt () {
+  crypt("decrypt", texto.value)
+};
 
 // Evento de botones
-let buttonEncriptar = document.getElementById("button-encrypt");  
-buttonEncriptar.addEventListener("click", starEncrypt);  //Apregado del evento para el inicio del encritado
+const buttonEncrypt = document.getElementById("button-encrypt");  
+buttonEncrypt.addEventListener("click", starEncrypt);  //Apregado del evento para el inicio del encritado
+
+const buttonDecrypt = document.getElementById("button-decrypt");
+buttonDecrypt.addEventListener("click", starDecrypt);  //Apregado del evento para el inicio del Desecriptado
