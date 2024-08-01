d22397817d3ebc2502dd429ee90ead7afecdc6a1
JorgeLeon066 <almendrasj315@gmail.com>
2024-08-01 18:01:23 -0300

    linplieza de codigo js

--- javascript/index.js
+++ javascript/index.js
@@ -1,58 +1,45 @@
-/*
-Requisitos:
-
-Debe funcionar solo con letras minúsculas
-
-No deben ser utilizados letras con acentos ni caracteres especiales
-
-Debe ser posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original
-*/
-const key = ["enter", "imes", "ai", "ober", "ufat"];
-
-const texto = document.getElementById("textarea-input");
-const outCrypt = document.getElementById('output-message');
-const messageNone = document.getElementById('message-none');
-
-//Funcionalidades
+const inputText = document.getElementById("textarea-input");
+const outputText = document.getElementById("textarea-output");
 
 const send = (txt) => {
-  messageNone.style.display = "none";
-  outCrypt.innerText = txt;
+  txt = txt.toLowerCase();
+  outputText.value = txt;  // Usar .value en lugar de .innerText
 }
 
 const encrypt = (txt) => {
-  for (i = 0; i < key.length; i++) {
+  const key = ["enter", "imes", "ai", "ober", "ufat"];
+
+  for (let i = 0; i < key.length; i++) {
     txt = txt.replaceAll(key[i][0], key[i]);
   };
   return txt;
 };
 
 const decrypt = (txt) => {
-  for (i = 0; i < key.length; i++) {
+  const key = ["enter", "imes", "ai", "ober", "ufat"];
+
+  for (let i = 0; i < key.length; i++) {
     txt = txt.replaceAll(key[i], key[i][0]);
   };
   return txt;
 }
 
-const crypt = (type, txt_edit) => {
-  send(
-    type === "encrypt" && encrypt(txt_edit) ||
-    type === "decrypt" && decrypt(txt_edit)
-  );
+const crypt = (type, txtEdit) => {
+  const sendMessage = type === "encrypt" ? encrypt(txtEdit) : decrypt(txtEdit);
+  send(sendMessage);
 }
 
-
-function starEncrypt () {
-  crypt("encrypt", texto.value)
+function starEncrypt() {
+  crypt("encrypt", inputText.value);
 };
 
-function starDecrypt () {
-  crypt("decrypt", texto.value)
+function starDecrypt() {
+  crypt("decrypt", inputText.value);
 };
 
-// Evento de botones
+// Eventos de botones
 const buttonEncrypt = document.getElementById("button-encrypt");  
-buttonEncrypt.addEventListener("click", starEncrypt);  //Apregado del evento para el inicio del encritado
+buttonEncrypt.addEventListener("click", starEncrypt);  // Agregado del evento para el inicio del encritado
 
 const buttonDecrypt = document.getElementById("button-decrypt");
-buttonDecrypt.addEventListener("click", starDecrypt);  //Apregado del evento para el inicio del Desecriptado
+buttonDecrypt.addEventListener("click", starDecrypt);  // Agregado del evento para el inicio del desencriptado
