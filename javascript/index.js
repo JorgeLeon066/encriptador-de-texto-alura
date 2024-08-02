e90d9d43a54e4996be055dd4ec73954c1917989b
JorgeLeon066 <almendrasj315@gmail.com>
2024-08-02 09:14:26 -0300

    Boton copiar completo y tipo de entrada

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,4 @@
-const inputText = document.getElementById("textarea-input");
-const outputText = document.getElementById("textarea-output");
-
 // Funciones Complementarias
-
-const send = (txt) => {
-  txt = txt.toLowerCase();
-  
-  outputText.style.backgroundImage = "none";
-  outputText.value = txt;  // Usar .value en lugar de .innerText
-}
-
 const encrypt = (txt) => {
   const key = ["enter", "imes", "ai", "ober", "ufat"];
 
@@ -26,15 +15,42 @@ const decrypt = (txt) => {
     txt = txt.replaceAll(key[i], key[i][0]);
   };
   return txt;
-}
+};
+
+const validation = txt => {
+  const regex = /^[a-z\s]*$/;
+  return !regex.test(txt);
+  // const conditon = "áéíóú";
+  // return [...txt].some(char => conditon.includes(char))
+};
+
+const sendAlert = (ult) => {
+  alert("Se detecto el INGRESO DE UN CARACTER NO VALIDO");
+  inputText.setAttribute("maxlength", `${ult}`);
+};
+
+const send = (txt) => {
+  txt = txt.toLowerCase();
+
+  outputText.style.backgroundImage = "none";
+  outputText.value = txt;  // Usar .value en lugar de .innerText
+};
 
 const crypt = (type, txtEdit) => {
   const sendMessage = type === "encrypt" ? encrypt(txtEdit) : decrypt(txtEdit);
   send(sendMessage);
+};
+
+const restValidation = () => {
+  const ult = inputText.value.length - 1;
+  const vall = validation(inputText.value)
+  // console.log(validation(inputText.value));
+
+  vall && sendAlert(ult);
+  inputText.hasAttribute("maxlength") && inputText.removeAttribute("maxlength");
 }
 
 // Principales funciones
-
 function starEncrypt() {
   crypt("encrypt", inputText.value);
 };
@@ -43,9 +59,36 @@ function starDecrypt() {
   crypt("decrypt", inputText.value);
 };
 
+function copyText() {
+  navigator.clipboard.writeText(outputText.value);
+};
+
+function inputCheck() {
+  const text = inputText.value;
+  const isValid = validation(text);
+  // console.log(validation(text));
+
+  if (isValid) {
+    const ult = text.length - 1;
+    sendAlert(ult);
+  } else {
+    inputText.removeAttribute("maxlength");
+  }
+};
+
 // Eventos de botones
-const buttonEncrypt = document.getElementById("button-encrypt");  
+const buttonEncrypt = document.getElementById("button-encrypt");
 buttonEncrypt.addEventListener("click", starEncrypt);  // Agregado del evento para el inicio del encritado
 
 const buttonDecrypt = document.getElementById("button-decrypt");
 buttonDecrypt.addEventListener("click", starDecrypt);  // Agregado del evento para el inicio del desencriptado
+
+const buttonCopy = document.getElementById('button-copy');
+buttonCopy.addEventListener('click', copyText);   // Agragado del evento para el copiado del texto de salida
+
+// Evento de lectura de entras de texto
+const inputText = document.getElementById("textarea-input");
+inputText.addEventListener("input", inputCheck);
+
+// Otros
+const outputText = document.getElementById("textarea-output");
